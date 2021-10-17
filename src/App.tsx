6ab4a1f66b406ceb6930c5995d71bf832f6080af
Balazs Nyirfa <balazsnyirfa@gmail.com>
2021-10-17 17:18:51 +0200

    add typescript, parcel, react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import Menu from "./components/Menu";
+import About from "./pages/About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const AppRouter = (
+    <Router>
+      <Menu />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {AppRouter}
     </div>
   );
 }
