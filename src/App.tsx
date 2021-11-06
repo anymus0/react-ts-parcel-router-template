8680875b6129f24daa3a989dc92da2d5ff85a7cb
Balazs Nyirfa <38496187+anymus0@users.noreply.github.com>
2021-11-06 10:48:23 +0100

    update AppRouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,17 @@ import "./App.css";
 import Home from "./pages/Home";
 import Menu from "./components/Menu";
 import About from "./pages/About";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const AppRouter = (
-    <Router>
-      <Menu />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-      </Switch>
-    </Router>
+  <Router>
+    <Menu></Menu>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+    </Routes>
+  </Router>
   );
 
   return (
